41eb42c0940bd5bfdfc185084c5e50f74bee1c99
Chan Zi Shen <zishen.chan@cpsd.lab>
2024-02-14 15:21:08 +0800

    Updated to test loop

--- testing123.py
+++ testing123.py
@@ -1,1 +1,15 @@
+from time import sleep
+filename = "output.txt"
+
 print("Hello")
+print("No")
+
+with open(filename, "a") as file:
+    for i in range(1,11):
+        print(str(i) + "written to file.")
+
+        file.write(str(i) + "\n")
+
+        sleep(10)
+
+print("All numbers printed and written to file!")
